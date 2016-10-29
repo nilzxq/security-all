37571a29b4944595ee98b0d0cf180f867cab266a
nilzxq <2013358717@qq.com>
2016-10-29 14:38:31 +0800

    dsa数字签名

---
+++ security-all/src/main/java/com/nilzxq/security/dsa/SecurityDSA.java
@@ -0,0 +1,63 @@
+package main.java.com.nilzxq.security.dsa;
+
+import java.security.KeyFactory;
+import java.security.KeyPairGenerator;
+import java.security.KeyPair;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.Signature;
+import java.security.interfaces.DSAPrivateKey;
+import java.security.interfaces.DSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+
+import org.apache.commons.codec.binary.Hex;
+
+/**
+ * @author zxq
+ *基本同rsa
+ */
+public class SecurityDSA {
+	private static String src = "nilzxq security base64";
+
+	public static void main(String[] args) {
+		jdkDSA();
+	}
+
+	public static void jdkDSA() {
+		// 1.初始化密钥
+		try {
+			KeyPairGenerator keyPairGenerator = KeyPairGenerator
+					.getInstance("DSA");
+			keyPairGenerator.initialize(512);
+			KeyPair keyPair = keyPairGenerator.generateKeyPair();
+			DSAPublicKey dsaPublicKey = (DSAPublicKey) keyPair.getPublic();
+			DSAPrivateKey dsaPrivateKey = (DSAPrivateKey) keyPair.getPrivate();
+
+			// 2.执行签名
+			PKCS8EncodedKeySpec pkcs8EncodedKeySpec = new PKCS8EncodedKeySpec(
+					dsaPrivateKey.getEncoded());
+			KeyFactory keyFactory = KeyFactory.getInstance("DSA");
+			PrivateKey privateKey = keyFactory
+					.generatePrivate(pkcs8EncodedKeySpec);
+			Signature signature = Signature.getInstance("SHA1withDSA");
+			signature.initSign(privateKey);
+			signature.update(src.getBytes());
+			byte[] result = signature.sign();
+			System.out.println("jdk dsa sign:" + Hex.encodeHexString(result));
+
+			// 3.验证签名
+			X509EncodedKeySpec x509EncodedKeySpec = new X509EncodedKeySpec(
+					dsaPublicKey.getEncoded());
+			keyFactory = KeyFactory.getInstance("DSA");
+			PublicKey publicKey = keyFactory.generatePublic(x509EncodedKeySpec);
+			signature = Signature.getInstance("SHA1withDSA");
+			signature.initVerify(publicKey);
+			signature.update(src.getBytes());
+			boolean bool = signature.verify(result);
+			System.out.println("jdk dsa verify:" + bool);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
